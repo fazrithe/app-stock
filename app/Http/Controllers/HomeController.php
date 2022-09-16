d67e7a59f90141fbe2776e73a5e9cef07f655690
MOH DEDE FAZRI <fazrithe@gmail.com>
2022-09-16 10:30:18 +0700

    Update access login
    Update access login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,17 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $auth = Auth::user()->roles->first()->name ;
+        if($auth == 'Admin'){
+            return view('home');
+        }else{
+            $data = [
+                'login_date' => $request->session()->get('login_date'),
+                'area'  => Auth::user()->area,
+            ];
+            return view('stock.index', compact('data'));
+        }
     }
 }

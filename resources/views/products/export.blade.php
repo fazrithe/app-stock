7390c77adfa43c4ebd9098a47f848ef07e5563da
MOH DEDE FAZRI <fazrithe@gmail.com>
2022-09-16 23:55:55 +0700

    update theme excel
    update theme excel

--- resources/views/products/export.blade.php
+++ resources/views/products/export.blade.php
@@ -1,34 +1,35 @@
 <table>
     <thead>
         <tr>
-            <th>Barcode</th>
-            <th>Merk</th>
-            <th>Kode Barang</th>
-            <th>Nama Barang</th>
-            <th>Satuan</th>
-            <th colspan="5">Toko</th>
-            <th>Total Toko</th>
-            <th colspan="5">Gudang</th>
-            <th>Total Gudang</th>
+            <th style="background-color: black; color:white"><p style="color: white">Barcode</p></th>
+            <th style="background-color: black; color:white"><label style="color: white">Merk</label></th>
+            <th style="background-color: black; color:white"><label style="color: white">Kode Barang</label></th>
+            <th style="background-color: red; color:white"><label style="color: white">Nama Barang</label></th>
+            <th style="background-color: gray; color:white"><label style="color: white">Satuan</label></th>
+            <th colspan="5" style="background-color: brown; color:white" align="center"><label style="color: white">Toko</label></th>
+            <th style="background-color: brown; color:white"><label style="color: white">Total Toko</label></th>
+            <th style="background-color: gray; color:white" colspan="5" align="center"><label style="color: white">Gudang</label></th>
+            <th style="background-color: gray; color:white"><label style="color: white">Total Gudang</label></th>
             <th>Tanggal</th>
         </tr>
         <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th>Toko 1</th>
-            <th>Toko 2</th>
-            <th>Toko 3</th>
-            <th>Toko 4</th>
-            <th>Toko 5</th>
-            <th></th>
-            <th>Gudang 1</th>
-            <th>Gudang 2</th>
-            <th>Gudang 3</th>
-            <th>Gudang 4</th>
-            <th>Gudang 5</th>
+            <th style="background-color: black"></th>
+            <th style="background-color: black"></th>
+            <th style="background-color: black"></th>
+            <th style="background-color: red"></th>
+            <th style="background-color: gray"></th>
+            <th style="background-color: brown; color:white">Toko 1</th>
+            <th style="background-color: brown; color:white">Toko 2</th>
+            <th style="background-color: brown; color:white">Toko 3</th>
+            <th style="background-color: brown; color:white">Toko 4</th>
+            <th style="background-color: brown; color:white">Toko 5</th>
+            <th style="background-color: brown"></th>
+            <th style="background-color: gray; color:white">Gudang 1</th>
+            <th style="background-color: gray;color:white">Gudang 2</th>
+            <th style="background-color: gray;color:white">Gudang 3</th>
+            <th style="background-color: gray;color:white">Gudang 4</th>
+            <th style="background-color: gray;color:white">Gudang 5</th>
+            <th style="background-color: gray"></th>
             <th></th>
         </tr>
     </thead>

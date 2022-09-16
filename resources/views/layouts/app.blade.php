7f9253d760440b744c1cecbdc9a6b314cd4ab8c8
MOH DEDE FAZRI <fazrithe@gmail.com>
2022-09-16 10:24:06 +0700

    Update icon
    Update icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ asset('assets/img/icon.png') }}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   @include('layouts.navbar');

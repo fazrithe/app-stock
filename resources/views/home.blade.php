fabb3ff2217b140a1b91dc20c9fe383105d54d11
MOH DEDE FAZRI <fazrithe@gmail.com>
2022-09-17 15:25:44 +0700

    Update dashboard
    Update dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
           <div class="inner">
             <h3>150</h3>
 
-            <p>New Orders</p>
+            <p>Stock Opname</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
@@ -23,9 +23,9 @@
         <!-- small box -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
+            <h3>2000<sup style="font-size: 20px"></sup></h3>
 
-            <p>Bounce Rate</p>
+            <p>Barang</p>
           </div>
           <div class="icon">
             <i class="ion ion-stats-bars"></i>
@@ -38,29 +38,14 @@
         <!-- small box -->
         <div class="small-box bg-warning">
           <div class="inner">
-            <h3>44</h3>
+            <h3>11</h3>
 
             <p>User Registrations</p>
           </div>
           <div class="icon">
             <i class="ion ion-person-add"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-danger">
-          <div class="inner">
-            <h3>65</h3>
-
-            <p>Unique Visitors</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-pie-graph"></i>
-          </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="{{ route('users.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <!-- ./col -->
